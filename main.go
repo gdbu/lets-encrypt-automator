cd35002963bccc21ff28a7c248f0a2a46721bbe9
Josh Montoya <josh@montoya.io>
2021-01-22 10:39:40 -0800

    implement main

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gdbu/lets-encrypt-server/certprocure"
+	"github.com/hatchify/closer"
+)
+
+// Init will be called by vroomy on initialization before Configure
+func main() {
+	var (
+		c   *certprocure.CertProcure
+		err error
+	)
+
+	if c, err = certprocure.New(); err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	cc := closer.New()
+
+	if err = cc.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
